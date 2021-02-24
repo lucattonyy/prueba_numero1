615b5b096f66e0c658ca5635f2bdfdd33ff64605
lucattonyy <adormilucasnunez@gmail.com>
2021-02-24 10:40:56 +0100

    configuracion de qtile

---
+++ config.py-main/config.py
@@ -0,0 +1,369 @@
+# Qtile keybindings
+
+from libqtile.config import Key
+from libqtile.command import lazy
+
+
+mod = "mod4"
+
+keys = [Key(key[0], key[1], *key[2:]) for key in [
+    # ------------ Window Configs ------------
+
+    # Switch between windows in current stack pane
+    ([mod], "j", lazy.layout.down()),
+    ([mod], "k", lazy.layout.up()),
+    ([mod], "h", lazy.layout.left()),
+    ([mod], "l", lazy.layout.right()),
+
+    # Change window sizes (MonadTall)
+    ([mod, "shift"], "l", lazy.layout.grow()),
+    ([mod, "shift"], "h", lazy.layout.shrink()),
+
+    # Toggle floating
+    ([mod, "shift"], "f", lazy.window.toggle_floating()),
+
+    # Move windows up or down in current stack
+    ([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    ([mod, "shift"], "k", lazy.layout.shuffle_up()),
+
+    # Toggle between different layouts as defined below
+    ([mod], "Tab", lazy.next_layout()),
+    ([mod, "shift"], "Tab", lazy.prev_layout()),
+
+    # Kill window
+    ([mod], "w", lazy.window.kill()),
+
+    # Switch focus of monitors
+    ([mod], "period", lazy.next_screen()),
+    ([mod], "comma", lazy.prev_screen()),
+
+    # Restart Qtile
+    ([mod, "control"], "r", lazy.restart()),
+
+    ([mod, "control"], "q", lazy.shutdown()),
+    ([mod], "r", lazy.spawncmd()),
+    
+    
+
+    # ------------ App Configs ------------
+
+    # Menu
+    ([mod], "m", lazy.spawn("rofi -show drun")),
+
+    # Window Nav
+    ([mod, "shift"], "m", lazy.spawn("rofi -show")),
+
+    # Browser
+    ([mod], "b", lazy.spawn("firefox")),
+
+    # File Explorer
+    ([mod], "f", lazy.spawn("pcmanfm")),
+
+    # Terminal
+    ([mod], "Return", lazy.spawn("alacritty")),
+
+    # Redshift
+    ([mod], "r", lazy.spawn("redshift -O 2400")),
+    ([mod, "shift"], "r", lazy.spawn("redshift -x")),
+
+    # Screenshot
+    ([mod], "s", lazy.spawn("scrot")),
+    
+    
+
+    # ------------ Hardware Configs ------------
+
+    # Volume
+    ([], "XF86AudioLowerVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ -5%"
+    )),
+    ([], "XF86AudioRaiseVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ +5%"
+    )),
+    ([], "XF86AudioMute", lazy.spawn(
+        "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+    )),
+
+    # Brightness
+    ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+    ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+]]
+
+
+
+# Qtile workspaces
+
+from libqtile.config import Key, Group
+from libqtile.command import lazy
+from settings.keys import mod, keys
+
+
+# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
+# Icons:
+# terminal,
+# firefox, 
+# python, 
+# html, 
+# database, 
+# arch linux, 
+# web, 
+# atom, 
+# twitch,
+# nf-mdi-layers
+
+groups = [Group(i) for i in [
+    "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ",
+]]
+
+for i, group in enumerate(groups):
+    actual_key = str(i + 1)
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
+    
+
+
+# Layouts and layout rules
+
+
+layout_conf = {
+    'border_focus': colors['focus'][0],
+    'border_width': 1,
+    'margin': 4
+}
+
+layouts = [
+    layout.Max(),
+    layout.MonadTall(**layout_conf),
+    layout.MonadWide(**layout_conf),
+    layout.Bsp(**layout_conf),
+    layout.Matrix(columns=2, **layout_conf),
+    layout.RatioTile(**layout_conf),
+    # layout.Columns(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+floating_layout = layout.Floating(
+    float_rules=[
+        {'wmclass': 'confirm'},
+        {'wmclass': 'dialog'},
+        {'wmclass': 'download'},
+        {'wmclass': 'error'},
+        {'wmclass': 'file_progress'},
+        {'wmclass': 'notification'},
+        {'wmclass': 'splash'},
+        {'wmclass': 'toolbar'},
+        {'wmclass': 'confirmreset'},
+        {'wmclass': 'makebranch'},
+        {'wmclass': 'maketag'},
+        {'wname': 'branchdialog'},
+        {'wname': 'pinentry'},
+        {'wmclass': 'ssh-askpass'},
+    ],
+    border_focus=colors["color4"][0]
+)
+
+
+from libqtile.config import Screen
+from libqtile import bar, layout
+import subprocess
+
+
+status_bar = lambda widgets: bar.Bar(widgets, 24, opacity=0.95)
+
+screens = [Screen(top=status_bar(primary_widgets))]
+
+connected_monitors = subprocess.run(
+    "xrandr | grep 'connected' | cut -d ' ' -f 2",
+    shell=True,
+    stdout=subprocess.PIPE
+).stdout.decode("UTF-8").split("\n")[:-1].count("connected")
+
+if connected_monitors > 1:
+    for i in range(1, connected_monitors):
+        screens.append(Screen(top=status_bar(secondary_widgets)))
+
+
+
+
+from libqtile.config import Drag, Click
+from libqtile.command import lazy
+from settings.keys import mod
+
+
+mouse = [
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position()
+    ),
+    Drag(
+        [mod],
+        "Button3",
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size()
+    ),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+
+
+from os import path
+
+qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
+
+
+
+
+# Theming for Qtile
+
+from os import path
+import subprocess
+import json
+
+from settings.path import qtile_path
+
+
+def load_theme():
+    theme = "dark-grey"
+
+    config = path.join(qtile_path, "config.json")
+    if path.isfile(config):
+        with open(config) as f:
+            theme = json.load(f)["theme"]
+    else:
+        with open(config, "w") as f:
+            f.write(f'{{"theme": "{theme}"}}\n')
+
+
+    theme_file = path.join(qtile_path, "themes", f'{theme}.json')
+    if not path.isfile(theme_file):
+        raise Exception(f'"{theme_file}" does not exist')
+
+    with open(path.join(theme_file)) as f:
+        return json.load(f)
+
+
+if __name__ == "settings.theme":
+    colors = load_theme()
+    
+    
+    
+from libqtile import widget
+from settings.theme import colors
+
+# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
+
+base = lambda fg='text', bg='dark': {
+    'foreground': colors[fg],
+    'background': colors[bg]
+}
+
+separator = lambda: widget.Sep(**base(), linewidth=0, padding=5)
+
+icon = lambda fg='text', bg='dark', fontsize=16, text="?": widget.TextBox(
+    **base(fg, bg),
+    fontsize=fontsize,
+    text=text,
+    padding=3
+)
+
+powerline = lambda fg="light", bg="dark": widget.TextBox(
+   **base(fg, bg),
+    text="", # Icon: nf-oct-triangle_left
+    fontsize=37,
+    padding=-2
+)
+
+workspaces = lambda: [
+    separator(),
+    widget.GroupBox(
+        **base(fg='light'),
+        font='UbuntuMono Nerd Font',
+        fontsize=19,
+        margin_y=3,
+        margin_x=0,
+        padding_y=8,
+        padding_x=5,
+        borderwidth=1,
+        active=colors['active'],
+        inactive=colors['inactive'],
+        rounded=False,
+        highlight_method='block',
+        urgent_alert_method='block',
+        urgent_border=colors['urgent'],
+        this_current_screen_border=colors['focus'],
+        this_screen_border=colors['grey'],
+        other_current_screen_border=colors['dark'],
+        other_screen_border=colors['dark'],
+        disable_drag=True
+    ),
+    separator(),
+    widget.WindowName(**base(fg='focus'), fontsize=14, padding=5),
+    separator(),
+]
+
+primary_widgets = [
+    *workspaces(),
+
+    separator(),
+
+    powerline('color4', 'dark'),
+
+    icon(bg="color4", text=' '), # Icon: nf-fa-download
+    
+    widget.Pacman(**base(bg='color4'), update_interval=1800),
+
+    powerline('color3', 'color4'),
+
+    icon(bg="color3", text=' '),  # Icon: nf-fa-feed
+    
+    widget.Net(**base(bg='color3'), interface='wlp2s0'),
+
+    powerline('color2', 'color3'),
+
+    widget.CurrentLayoutIcon(**base(bg='color2'), scale=0.65),
+
+    widget.CurrentLayout(**base(bg='color2'), padding=5),
+
+    powerline('color1', 'color2'),
+
+    icon(bg="color1", fontsize=17, text=' '), # Icon: nf-mdi-calendar_clock
+
+    widget.Clock(**base(bg='color1'), format='%d/%m/%Y - %H:%M '),
+
+    powerline('dark', 'color1'),
+
+    widget.Systray(background=colors['dark'], padding=5),
+
+]
+
+secondary_widgets = [
+    *workspaces(),
+
+    separator(),
+
+    powerline('color1', 'dark'),
+
+    widget.CurrentLayoutIcon(**base(bg='color1'), scale=0.65),
+
+    widget.CurrentLayout(**base(bg='color1'), padding=5),
+]
+
+widget_defaults = {
+    'font': 'UbuntuMono Nerd Font Bold',
+    'fontsize': 14,
+    'padding': 1,
+}
+extension_defaults = widget_defaults.copy()
+
+
+
+
